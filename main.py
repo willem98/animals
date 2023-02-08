8373649cc652b91bba5a78a181fb7108737b8c87
AloneInSky <stephenonty@gmail.com>
2023-02-08 22:36:02 +0700

    class

--- main.py
+++ main.py
@@ -8,25 +8,25 @@ from flet import Image
 from flet import UserControl
 
 class Kelas(UserControl):
-    def __init__(self,i, page) -> None:
+    def __init__(self,i, page, ) -> None:
         super().__init__()
         self.i = i
         self.page = page
 
-    def kelaspage(self,):
+    def kelaspage(self, e):
+        self.page.clean()
+        self.page.add(txt(self.i))
         return self.page.reload()
 
     def build(self):
+        print(self.i,self.page)
         return button(self.i,on_click=self.kelaspage)
 
 
 def main(page: Page):
     page.add(Kelas("Serangga",page))
-    btn = ft.ElevatedButton("Mamalia")
-    btn = ft.ElevatedButton("Reptill")
-    btn = ft.ElevatedButton("Unggas")
-    page.add(Kelas("Serangga",page))
-    page.add(Kelas("Serangga",page))
-    page.add(Kelas("Serangga",page))
+    page.add(Kelas("Mamalia",page))
+    page.add(Kelas("Reptil",page))
+    page.add(Kelas("Unggas",page))
 
 app(target=main)
