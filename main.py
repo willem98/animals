b32f83523b26d110d950e2c5297b9a6312de31f6
Willem E. Humau <suahumau46@gmail.com>
2023-02-08 23:10:22 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import flet as ft
+
+def main(page: ft.Page):
+    btn = ft.ElevatedButton("Mamalia")
+    page.add(btn)
+
+def main(page: ft.Page):
+    btn = ft.ElevatedButton("Reptill")
+    page.add(btn)
+
+def main(page: ft.Page):
+    btn = ft.ElevatedButton("Unggas")
+    page.add(btn)
+
+def main(page: ft.Page):
+    btn = ft.ElevatedButton("Serangga")
+    page.add(btn)
+
+ft.app(target=main)
