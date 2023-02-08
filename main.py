c97f9c919a79035eeb725b54e9e55800ba8373bf
AloneInSky <stephenonty@gmail.com>
2023-02-08 22:24:00 +0700

    object

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ from flet import ElevatedButton as button
 from flet import Image
 import flet as ft
 
+class Kelas(ft.UserControl):
+    def __init__(self,i, page) -> None:
+        super().__init__()
+        self.i = i
+        self.page = page
+
+    def kelaspage(self,):
+        return self.page.reload()
+
+    def build(self):
+        return button(self.txt,on_click=self.kelaspage)
+
 def main(page: ft.Page):
     btn = ft.ElevatedButton("Mamalia")
     page.add(btn)
